0e6a7031230b54cc34920f741e0ec8f044d6b7f7
jitesh456 <jitesh0dubey@gmail.com>
2020-01-30 17:00:14 +0530

    ADDED:function for changing player position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,10 +1,27 @@
+
 #!/bin/bash -x
 echo "================== Welcome to Snake Ladder Game ==================="
 playerPosition=0;
-
+snake=1;
+ladder=2;
+noPlay=0;
 function die()
 {
    echo $(($((RANDOM %6))+1))
 }
-die
-
+function	playerMove()
+{
+   dieNo=$(die )
+   case $((RANDOM %3)) in
+      $snake)
+               playerPosition=$(($playerPosition-$dieNo))
+               ;;
+      $ladder)
+               playerPosition=$(($playerPosition+$dieNo))
+               ;;
+      $noPlay)
+               
+               ;;
+   esac
+}
+playerMove

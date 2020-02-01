8ad4d15f87adfe6a529e69b4860258c3f6570c09
jitesh456 <jitesh0dubey@gmail.com>
2020-02-01 10:01:19 +0530

    REFACTORED:created different function for checking playerPosition

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,22 +1,40 @@
-#!/bin/bash -x
+#!/bin/bash  
 echo "================== Welcome to Snake Ladder Game ==================="
 
 WINNING_POSITION=100;
-
 playerPosition=0;
+firstPlayer=0
 snake=1;
 ladder=2;
 noPlay=0;
+count=0
 
+
+declare -A playerRecord
 function die()
 {
    echo $(($((RANDOM %6))+1))
 }
+function condiationCheck
+{
+	position=$1
 
+	if (( $position < 0 ))
+	then
+			position=0;
+	fi
+	if (( $position>100 ))
+	then
+        position=$(($position-$2))
+	fi
+	echo $position
+
+}
 function	playerMove()
 {
-	dieNo=$(die)
-
+	dieNo=$(die )
+	((count++))
+		playerPosition=0
 		case $((RANDOM %3)) in
 			$snake)
 				playerPosition=$(($playerPosition-$dieNo))
@@ -28,20 +46,13 @@ function	playerMove()
 					;;
 		esac
 
-		if (( $playerPosition < 0 ))
+		if (( $flag==0 ))
 		then
-			playerPosition=0;
-		fi
-	
-		if (( $playerPosition>100 ))
-		then
-			playerPosition=$(($playerPosition-$dieNo))
-		fi
+			firstPlayer=$(($firstPlayer +$playerPosition))
+			firstPlayer=$(condiationCheck $firstPlayer $dieNo)
+			playerRecord[DieNo:"$dieNo"]="player 1:$firstPlayer"
 }
-
 while (( $playerPosition != $WINNING_POSITION ))
 do
-		playerMove
-		echo $playerPosition
-
+		playerMove 
 done
